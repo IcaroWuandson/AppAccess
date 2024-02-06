57e7aff7edebfc15280eeb645572f710b4421404
Ícaro Wuandson <icarowuandson1@gmail.com>
2024-02-06 18:17:00 -0300

    Ajustar camera pedida pela apple

--- src/Chat/index.js
+++ src/Chat/index.js
@@ -2,8 +2,18 @@ import * as React from "react";
 import { WebView } from "react-native-webview";
 import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Camera, CameraType } from "expo-camera";
 
 export default function Chat() {
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+
+  function toggleCameraType() {
+    setType((current) =>
+      current === CameraType.back ? CameraType.front : CameraType.back
+    );
+  }
+
   return (
     <SafeAreaView style={styles.containerPage}>
       <WebView
