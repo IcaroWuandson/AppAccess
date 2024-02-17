336448b86b5e093253940f3eea6dc6bff0a7ae07
Ícaro Wuandson <icarowuandson1@gmail.com>
2024-02-17 08:39:42 -0300

    corrigir bug da tela de chat

--- src/Chat/index.js
+++ src/Chat/index.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { WebView } from "react-native-webview";
 import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
